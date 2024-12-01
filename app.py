60621c84415c502cbd72d9790f92dfd6406708fe
JemyYousef <jemy171293@gmail.com>
2024-12-01 22:17:13 +0200

    REST-API APP

--- app.py
+++ app.py
@@ -2,22 +2,14 @@ from flask import Flask, jsonify
 import requests
 from datetime import datetime, timedelta
 from prometheus_client import generate_latest, Gauge
-
 app = Flask(__name__)
-
 TEMPERATURE_GAUGE = Gauge('average_temperature', 'Average temperature of senseBox sensors')
-
-
 @app.route('/version', methods=['GET'])
 def version():
     return jsonify({'version': 'v0.0.1'})
-
-
 @app.route('/metrics', methods=['GET'])
 def metrics():
     return generate_latest()
-
-
 @app.route('/temperature', methods=['GET'])
 def temperature():
     senseBox_ids = [
@@ -25,10 +17,8 @@ def temperature():
         '5eba5fbad46fb8001b799787',
         '5eba5fbad46fb8001b799788'
     ]  # Add more IDs as needed
-
     temperatures = []
     current_time = datetime.utcnow()
-
     for senseBox_id in senseBox_ids:
         response = requests.get(f'https://api.opensensemap.org/boxes/{senseBox_id}')
         if response.status_code == 200:
@@ -49,13 +39,9 @@ def temperature():
                 f"Failed to fetch data for senseBox ID {senseBox_id} "
                 f"with status code {response.status_code}"
             )
-
     avg_temp = sum(temperatures) / len(temperatures) if temperatures else 0
     TEMPERATURE_GAUGE.set(avg_temp)
-    status = 'Too Cold' if avg_temp < 10 else 'Good' if avg_temp <= 36 else 'Too Hot'
-    
+    status = 'Too Cold' if avg_temp < 10 else 'Good' if avg_temp <= 36 else 'Too Hot'   
     return jsonify({'average_temperature': avg_temp, 'status': status})
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
