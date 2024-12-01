761568da9cd5423d19a91dce51b8f9fbbeff56f5
JemyYousef <jemy171293@gmail.com>
2024-12-01 23:14:36 +0200

    Last Modification

--- test_app.py
+++ test_app.py
@@ -18,4 +18,32 @@ def test_temperature():
         assert response.status_code == 200
         data = response.json()
         assert 'average_temperature' in data
+        assert 'status' in dataimport os
+import requests
+from app import app
+
+
+def test_version():
+    response = requests.get('http://localhost:5000/version')
+    assert response.status_code == 200
+    assert response.json() == {'version': 'v0.0.1'}
+
+
+def test_metrics():
+    response = requests.get('http://localhost:5000/metrics')
+    assert response.status_code == 200
+
+
+def test_temperature():
+    os.environ['SENSEBOX_IDS'] = (
+    '5eba5fbad46fb8001b799786,'
+    '5eba5fbad46fb8001b799787,'
+    '5eba5fbad46fb8001b799788'
+)
+    with app.test_client() as client:
+        response = client.get('/temperature')
+        assert response.status_code == 200
+        data = response.json()
+        assert 'average_temperature' in data
         assert 'status' in data
+
