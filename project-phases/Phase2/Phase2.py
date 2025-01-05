79b4ad30dc286beedd305f8235a808d03829a466
JemyYousef <jemy171293@gmail.com>
2025-01-05 23:31:19 +0200

    Recently Modified

--- project-phases/Phase2/Phase2.py
+++ project-phases/Phase2/Phase2.py
@@ -1,5 +1,7 @@
 def print_version():
     version = "v0.0.1"
     print(f"Current Application Version is {version}")
-if __name__ == "__main__" :
+
+
+if __name__ == "__main__":
     print_version()
